f6cb9396f4263e398f5d23985cf4a874cc4557a2
Jayti Gokharu <jayti.gokharu@pal.tech>
2026-09-04 14:10:42 +0530

    Add 404.html and router basename for GitHub Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Research-Agent">
       <Routes>
         <Route path="/" element={<ConfigurationPage />} />
         <Route path="/research/:configId" element={<ResearchResults />} />
